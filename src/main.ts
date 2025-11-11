a094b2601d13cc1bc20541d1923c9d58d16ce320
Aleksandar <bosnjakaleksandar02@gmail.com>
2025-11-11 22:21:48 +0100

    fix(SEO): seo optimizer tasks

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import './scss/main.scss';
-import { initSVGLoader } from './ts/loadSVG';
 import { initBurgerMenu } from './ts/components/hamburger';
 import { initHeaderScrollAnimation } from './ts/components/header';
 import { initCustomSelect } from './ts/components/customSelect';
 import { initRatesTable } from './ts/components/ratesTable';
 
-initSVGLoader();
 initBurgerMenu();
 initHeaderScrollAnimation();
 initCustomSelect();
