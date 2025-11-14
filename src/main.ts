93afe1e7b44f7d68a4e796122f91dadd1b0070d7
Aleksandar <bosnjakaleksandar02@gmail.com>
2025-11-14 19:44:44 +0100

    Optimization

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
-import './scss/main.scss';
 import { initBurgerMenu } from './ts/components/hamburger';
 import { initHeaderScrollAnimation } from './ts/components/header';
 import { initCustomSelect } from './ts/components/customSelect';
-import { initRatesTable } from './ts/components/ratesTable';
 import accordion from './ts/components/accordion';
 
-initBurgerMenu();
-initHeaderScrollAnimation();
-initCustomSelect();
-accordion();
+if (document.querySelector('header')) {
+  initBurgerMenu();
+}
 
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', initRatesTable);
-} else {
-  initRatesTable();
+if (document.querySelector('header')) {
+  initHeaderScrollAnimation();
+}
+
+if (document.querySelector('select')) {
+  initCustomSelect();
+}
+
+if (document.querySelector('.accordion') || document.querySelector('.js-accordion')) {
+  accordion();
+}
+
+if (document.getElementById('rates-table')) {
+  import('./ts/components/ratesTable').then(({ initRatesTable }) => {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', initRatesTable);
+    } else {
+      initRatesTable();
+    }
+  });
 }
