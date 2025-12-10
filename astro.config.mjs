f67d658e3f037580aa52765bf0dfdb8642b0ebec
Aleksandar <bosnjakaleksandar02@gmail.com>
2025-12-10 19:35:09 +0100

    fix(Image): image change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
-
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
