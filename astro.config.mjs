51d983f01b8fcbc72a47585ffda325fd67635d57
Aleksandar <bosnjakaleksandar02@gmail.com>
2026-04-12 23:39:35 +0200

    fix(TrailingSlash): seo issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://menjacnicagenes.rs',
+  trailingSlash: 'never',
   output: 'server',
   adapter: netlify({
     edgeMiddleware: true,
